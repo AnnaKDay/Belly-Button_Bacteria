09ae87eb1e1e1f1cdb47bdd6239ecf380738e153
AnnaKDay <katsukoday@gmail.com>
2022-06-05 18:34:20 -0500

    belly button bacteria data can be viewed via dropdown menu, by anonymous sample id

---
+++ Bacteria_Analysis/static/js/plots.js
@@ -0,0 +1,50 @@
+const url = "https://api.spacexdata.com/v2/launchpads";
+
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
+
+// creates a dropdown menu of ID numbers dynamically
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+// option changed function
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+};
+
+function buildMetadata(sample) {
+d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    Object.entries(result).forEach(([key, value]) => 
+        {PANEL.append("h6").text(key.toUpperCase() + ': ' + value);});
+   
+   
+});
+};
+
